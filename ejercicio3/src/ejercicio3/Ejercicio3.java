7aaf1331d263499581211b34853f70eb76f310e7
AlOlmo <berto.olmo@gmail.com>
2021-10-05 00:05:57 +0200

    Se ha añadido el ejercicio 3

---
+++ ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,41 @@
+package ejercicio3;
+
+/**
+ *
+ * @author Javier Otero Sobrino
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+        int número = 10;
+        int resultado = 5 * ++número;
+
+        System.out.println(resultado);
+
+        número = 10;
+        resultado = número++;
+        System.out.println(resultado);
+
+        número = 10;
+        resultado = --número;
+        System.out.println(resultado);
+
+        número = 10;
+        resultado = 5 * número++;
+        System.out.println(resultado);
+
+        número = 10;
+        resultado = número + 5;
+        System.out.println(resultado);
+        
+        número = 10;
+        resultado += 5;
+        System.out.println(resultado);
+        
+        resultado = 12 + 8 / 4;
+        System.out.println(resultado);
+
+    }
+
+}
